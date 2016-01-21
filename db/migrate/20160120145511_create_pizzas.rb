2a4c7743261cfb2ad4ae15f44f6f7c905655c312
PaigeWerhan <paige.werhan@gmail.com>
2016-01-21 08:34:17 -0500

    initial commit

---
+++ db/migrate/20160120145511_create_pizzas.rb
@@ -0,0 +1,15 @@
+class CreatePizzas < ActiveRecord::Migration
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.string :crust
+      t.string :string
+      t.string :sauce
+      t.text :toppings
+      t.integer :cost
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
